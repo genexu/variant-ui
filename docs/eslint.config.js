1c974fbd6bafd9eeacf6b89b9ae943be7e98da9a
Gene Xu <genexu1117.dev@gmail.com>
2024-10-15 20:50:38 +0800

    chore: setup prettier for docs

--- docs/eslint.config.js
+++ docs/eslint.config.js
@@ -1,28 +1,32 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  eslintConfigPrettier,
+  eslintPluginPrettierRecommended,
   {
     settings: {
       react: {
-        version: "detect",
-      }
-    }
+        version: 'detect',
+      },
+    },
   },
   {
     rules: {
-      "react/react-in-jsx-scope": "off",
-    }
+      'prettier/prettier': ['error'],
+      'react/react-in-jsx-scope': 'off',
+    },
   },
   {
-    ignores: ["node_modules", "dist"],
-  }
+    ignores: ['node_modules', 'dist'],
+  },
 ];
--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -1,7 +1,7 @@
-import { HelloWorld } from '@variant-ui/react'
-import theme from '@variant-ui/material-theme'
+import { HelloWorld } from '@variant-ui/react';
+import theme from '@variant-ui/material-theme';
 
-const ColorBlock = ({ color, name }: { color: string, name?: string }) => (
+const ColorBlock = ({ color, name }: { color: string; name?: string }) => (
   <div>
     <p>{name}</p>
     <div
@@ -12,24 +12,27 @@ const ColorBlock = ({ color, name }: { color: string, name?: string }) => (
       }}
     />
   </div>
-)
+);
 
-const ColorScale = ({ color }: { color: keyof typeof theme.colors.palette }) => {
-    const index = [100, 200, 300, 400, 500, 600, 700, 800, 900] as const
-    const palette = theme.colors.palette[color]
+const ColorScale = ({
+  color,
+}: {
+  color: keyof typeof theme.colors.palette;
+}) => {
+  const index = [100, 200, 300, 400, 500, 600, 700, 800, 900] as const;
+  const palette = theme.colors.palette[color];
 
-    return (
-      <>
-        <p>{color}</p>
-        <div style={{ display: 'flex' }}>
-          {index.map((i) => (
-            <ColorBlock key={i} color={palette[i]} />
-          ))}
-        </div>
-      </>
-    )
-
-}
+  return (
+    <>
+      <p>{color}</p>
+      <div style={{ display: 'flex' }}>
+        {index.map((i) => (
+          <ColorBlock key={i} color={palette[i]} />
+        ))}
+      </div>
+    </>
+  );
+};
 
 const App = () => {
   const colors = theme.colors;
@@ -38,20 +41,18 @@ const App = () => {
     <>
       <HelloWorld name="Tester" />
       <div style={{ display: 'flex' }}>
-      <ColorBlock color={colors.primary} name="primary" />
-      <ColorBlock color={colors.secondary} name="secondary" />
-      <ColorBlock color={colors.success} name="success" />
-      <ColorBlock color={colors.error} name="error" />
-      <ColorBlock color={colors.warning} name="warning" />
-      <ColorBlock color={colors.info} name="info" />
+        <ColorBlock color={colors.primary} name="primary" />
+        <ColorBlock color={colors.secondary} name="secondary" />
+        <ColorBlock color={colors.success} name="success" />
+        <ColorBlock color={colors.error} name="error" />
+        <ColorBlock color={colors.warning} name="warning" />
+        <ColorBlock color={colors.info} name="info" />
       </div>
-      {
-        Object.keys(colors.palette).map((color) => (
-          <ColorScale key={color} color={color as keyof typeof colors.palette} />
-        ))
-      }
+      {Object.keys(colors.palette).map((color) => (
+        <ColorScale key={color} color={color as keyof typeof colors.palette} />
+      ))}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
