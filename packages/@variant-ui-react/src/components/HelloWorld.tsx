5e4e0edf7cc9ae3a5345f842cd06ada1ffe87e79
Gene Xu <genexu1117.dev@gmail.com>
2024-10-15 21:56:21 +0800

    chore: update eslint setting and setup prettier for @variant-ui-react

--- packages/@variant-ui-react/src/components/HelloWorld.tsx
+++ packages/@variant-ui-react/src/components/HelloWorld.tsx
@@ -1,6 +1,8 @@
-export type THelloWorldProps = { name?: string } 
-export type THelloWorld = (props: THelloWorldProps) => JSX.Element
+export type THelloWorldProps = { name?: string };
+export type THelloWorld = (props: THelloWorldProps) => JSX.Element;
 
-export const HelloWorld: THelloWorld = ({ name = 'World' }) => {
-  return <h1>Hello {name}! This is @variant-ui/react!</h1>
-}
+export const HelloWorld: THelloWorld = ({
+  name = 'World',
+}: THelloWorldProps) => {
+  return <h1>Hello {name}! This is @variant-ui/react!</h1>;
+};
--- packages/@variant-ui-react/vite.config.ts
+++ packages/@variant-ui-react/vite.config.ts
@@ -1,23 +1,25 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
-import react from '@vitejs/plugin-react'
-import dts from 'vite-plugin-dts'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [react(), dts({
-    rollupTypes: true,
-    insertTypesEntry: true,
-    tsconfigPath: "./tsconfig.app.json",
-  })],
+  plugins: [
+    react(),
+    dts({
+      rollupTypes: true,
+      insertTypesEntry: true,
+      tsconfigPath: './tsconfig.app.json',
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       formats: ['es'],
-      fileName:  'index'
+      fileName: 'index',
     },
     watch: {
-      include: ['src/**']
+      include: ['src/**'],
     },
-  }
-})
-
+  },
+});
